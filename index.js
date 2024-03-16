f3ebe6953e83ab62ed4026efd74346b1c2506e86
trieu <dinh.qtrieu10@gmail.com>
2024-03-16 16:21:16 +0700

    main

--- index.js
+++ index.js
@@ -27,3 +27,56 @@ server.listen(PORT, () => {
 
 app.use(indexRoutes);
 app.use(authRoutes);
+
+
+const ModbusRTU = require('modbus-serial');
+const client = new ModbusRTU();
+const serialPort = 'COM3'; // Cổng COM RS485
+const options = {
+  baudRate: 9600, // Tốc độ truyền dữ liệu
+  dataBits: 8,    // Số bit dữ liệu
+  stopBits: 1,    // Số bit dừng
+  parity: 'none'  // Kiểu kiểm tra lỗi
+};
+
+// Kết nối với thiết bị Modbus qua cổng COM
+client.connectRTU(serialPort, options, connected);
+var dataArr = []; // Mảng chung để lưu trữ giá trị
+// var dataArr1 = []; // Mảng chung để lưu trữ giá trị
+
+async function connected() {
+    console.log('Đã kết nối với thiết bị Modbus qua cổng COM');
+    const unitId = 1; // Địa chỉ thiết bị Modbus
+    const addresses = [8192,8193,8194,8195,8196]; // ĐỌC CƠ SỐ 16
+    const quantity = 1; // Số lượng thanh ghi cần đọc
+    client.setID(unitId);
+
+    for (let i = 0; i < addresses.length; i++) {
+        const startAddress = addresses[i];
+        try {
+            const data = await readdata(startAddress, quantity);
+            dataArr[i] = data;
+            // dataArr1[0] = dataArr[1] + dataArr[2];
+        } catch (err) {
+            console.error('Lỗi khi đọc dữ liệu Modbus là:', err);
+        }
+    }
+    console.log('Dữ liệu Modbus:', dataArr);
+    // console.log('Dữ liệu Modbus1:', dataArr1);
+}
+
+setInterval(() => {
+    connected();
+}, 500);
+
+function readdata(startAddress, quantity) {
+    return new Promise((resolve, reject) => {
+        client.readHoldingRegisters(startAddress, quantity)
+            .then(data => {
+                resolve(data.data[0]);
+            })
+            .catch(err => {
+                reject(err);
+            });
+    });
+}
