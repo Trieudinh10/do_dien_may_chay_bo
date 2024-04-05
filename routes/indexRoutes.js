80de1cbf1c3bdfb1fa2960533253c15e91e5778e
vovanquynh-max <vovanquynh2000@gmail.com>
2024-04-05 13:22:06 +0700

    Q_edit

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 router.get('/', (req, res) => res.render('home'));
 router.get('/diennang', (req, res) => res.render('diennang'));
+router.get('/nhietdo', (req, res) => res.render('nhietdo'));
 router.get('/dulieu', (req, res) => res.render('dulieu'));
+router.get('/dulieu_temp', (req, res) => res.render('dulieu_temp'));
 router.get('/user', (req, res) => res.render('user'));
 module.exports = router;
